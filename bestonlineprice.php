6ea724bb0d5131fea0356939991ba68a3e2978a5
patryk <patryk.nietyksza@gmail.com>
2023-05-21 16:49:50 +0200

    first commit

---
+++ bestonlineprice.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+ * 2007-2023 PrestaShop
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/afl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to http://www.prestashop.com for more information.
+ *
+ *  @author    PrestaShop SA <contact@prestashop.com>
+ *  @copyright 2007-2023 PrestaShop SA
+ *  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ *  International Registered Trademark & Property of PrestaShop SA
+ */
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+class Bestonlineprice extends Module
+{
+    protected $config_form = false;
+    public const TEXT = 'Best price online';
+
+    public function __construct()
+    {
+        $this->name = 'bestonlineprice';
+        $this->tab = 'administration';
+        $this->version = '1.0.0';
+        $this->author = 'Patryk';
+        $this->need_instance = 0;
+        $this->bootstrap = true;
+        parent::__construct();
+        $this->displayName = $this->l('Best online Price');
+        $this->description = $this->l('Best online Price for products');
+
+        $this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
+    }
+
+    public function install()
+    {
+        Configuration::updateValue('BESTONLINEPRICE_LIVE_MODE', false);
+
+        return parent::install() &&
+            $this->registerHook('actionProductFlagsModifier');
+    }
+
+    public function uninstall()
+    {
+        Configuration::deleteByName('BESTONLINEPRICE_LIVE_MODE');
+
+        return parent::uninstall();
+    }
+
+    public function hookActionProductFlagsModifier($params)
+    {
+        if ($params['product']['price_amount'] <= 100) {
+            $params['flags']['bestprice'] = [
+                'type' => 'new',
+                'label' => $this->l(self::TEXT)
+            ];
+        }
+    }
+}
